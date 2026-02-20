38090b9a6c5de2aea77ad5cb262f7c49263902d4
Chakradhar Girhepunje <chakradhargirhepunje43@gmail.com>
2026-02-20 15:56:35 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -23,14 +23,19 @@ async function getSongs(folder) {
 
     // ✅ FIXED
     let a = await fetch(`/songs/${folder}/info.json`)
-    let data = await a.json();
+    let response = await a.text();
     let div = document.createElement("div")
     div.innerHTML = response;
     let as = div.getElementsByTagName("a")
 
     songs = []
 
-    songs = data.songs;
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href.split(`/songs/${folder}/`)[1])
+        }
+    }
 
     let songUL = document.querySelector(".songlist ul")
     songUL.innerHTML = ""
